dd79cafcb098ddd141b28e9eb7901e28798da33c
tequilex <dron2010xxx@mail.ru>
2026-04-14 12:32:57 +0300

    change next settings

--- tests/unit/config/next-config.test.ts
+++ tests/unit/config/next-config.test.ts
@@ -2,5 +2,6 @@ import nextConfig from '../../../next.config';
 
 test('next config is compatible with static export hosting', () => {
   expect(nextConfig.output).toBe('export');
+  expect(nextConfig.trailingSlash).toBe(true);
   expect(nextConfig.images?.unoptimized).toBe(true);
 });
